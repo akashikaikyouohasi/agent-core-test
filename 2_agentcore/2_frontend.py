19130b9485e7c42d97d77000705b72fc92b6e1e6
anikinthos <hayatata@live.jp>
2025-09-15 00:45:31 +0900

    streamlitの確認画面追加

---
+++ 2_agentcore/2_frontend.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os, asyncio, boto3, json, uuid
+import streamlit as st
+
+# .envファイルから環境変数をロード
+load_dotenv(dotenv_path="../.env")
+
+# タイトル
+st.title("Strands on AgentCore")
+st.write("何でも聞いてね！")
+
+# チャットボックスを描画
+if prompt := st.chat_input("メッセージを入力してね"):
+    # ユーザーのプロンプトを表示
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # エージェントの回答を表示
+    with st.chat_message("assistant"):
+        # AgentCoreランタイム呼び出し
+        with st.spinner("考え中..."):
+            agentcore = boto3.client("bedrock-agentcore")
+            response = agentcore.invoke_agent_runtime(
+                agentRuntimeArn=os.getenv("AGENT_RUNTIME_ARN"),
+                payload=json.dumps({"prompt": prompt})
+            )
+        
+        # 結果のテキストを取り出して表示
+        response_body = response["response"].read()
+        response_data = json.loads(response_body.decode("utf-8"))
+        st.write(response_data["result"]["content"][0]["text"])
+
+
